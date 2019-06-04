c051dd5b872cd6ca54c9b273aff13b6dc158fc81
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-04 14:52:43 -0700

    get products from data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,60 @@
 console.log("Hi")
 
 
-// get products
-let url = "https://kanjamadapishopping.herokuapp.com/api/products";
+var productsData;
 
-$.ajax({
-    method:"GET",
-    url: url,
-    dataType: "json"
-}).done(gotData)
+$(document).ready(function(){
+    // -------------------- products ---------------------
+    $productsData = $('#productsTarget');
+    $.ajax({
+        method: "GET",
+        url: "https://kanjamadapishopping.herokuapp.com/api/products",
+        success: handleSuccess,
+        error: handleError
+    });
 
-function gotData(data) {
-    console.log(data)
-    // $each(data, workData)
-}
+});
 
-// function workData(index, dataSET) {
-//     $('#main').append('<div class="lead">' + dataSET.images + "this is images from data" + index +  '</div>')
-// }
+// ----------------------- products ------------------------
+
+function getProductHtml(product){
+    return `
+        <!-- single product -->
+        <div class="col-10 mx-auto col-md-6 col-lg-4">
+            <div class="featured-container p-5">
+                <img src="${product.images[1]}" alt="${product.productName}"/>
+                <!-- <span class="featured-search-icon" 
+                data-toggle="modal" 
+                data-target="#productModal"><i class="fas fa-search"></i></span> -->
+                <a href="#" class="featured-store-link text-captilaze">add to cart</a>
+            </div>
+            <h6 class="text-capitalize text-center my-2">${product.productName}</h6>
+            <h6 class="text-center"><span class="text-muted old-price mx-2">${product.oldPrice}</span><span>$${product.price}</span></h6>
+        </div>
+        <!-- end single product -->
+    
+    `
+};
+
+function getAllProductHtml(products){
+    console.log(products)
+    return products.map(getProductHtml).join("");
+};
+
+function renderProduct(productsArr){
+    $productsData.empty();
+    const productsHtml = getAllProductHtml(productsArr);
+    $productsData.append(productsHtml);
+};
+
+function handleSuccess(json){
+    console.log(json.data);
+    renderProduct(json.data);
+};
+
+
+function handleError(e){
+    console.log('uh oh');
+    $('#productsTarget').text('Failed to load products, is the server working?');
+};
+
