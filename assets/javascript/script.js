c200e90259a538fdec9d9416c4d01d0f581788ec
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-04 12:09:23 -0700

    working on dropdown

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,20 @@
-console.log("Hi");
+console.log("Hi")
+
+
+// get products
+let url = "https://kanjamadapishopping.herokuapp.com/api/products";
+
+$.ajax({
+    method:"GET",
+    url: url,
+    dataType: "json"
+}).done(gotData)
+
+function gotData(data) {
+    console.log(data)
+    // $each(data, workData)
+}
+
+// function workData(index, dataSET) {
+//     $('#main').append('<div class="lead">' + dataSET.images + "this is images from data" + index +  '</div>')
+// }
